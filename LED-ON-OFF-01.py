ba77826b29c9fb03dbef200c0b0d45d25f4bc0ac
imranasghar <ashgar.imran@gmail.com>
2023-02-01 21:58:33 +0100

    Update LED-ON-OFF-01.py

--- LED-ON-OFF-01.py
+++ LED-ON-OFF-01.py
@@ -1,10 +1,11 @@
+## Install following module on RPi via commandline 
 ## sudo apt-get install python-rpi.gpio python3-rpi.gpio 
 ## importing GPIO module with alias
 import RPi.GPIO as  GPIO  
 import time
 
 GPIO.setwarnings(False)
- ## Set mode for the entire GPIO mode
+# Set mode for the entire GPIO mode
 GPIO.setmode(GPIO.BCM)  
 GPIO.setup(17,GPIO.OUT)
 
@@ -14,7 +15,7 @@ LED_PIN = 17
 print("Turning on the LED")
 GPIO.output(17, GPIO.HIGH)   ## Power on the LED 
 time.sleep(1)
-print("Turning off the LED")                     # Sleep timer, remember to import the time module import
+print("Turning off the LED")                     # Sleep timer, remember to import the time module; import time
 GPIO.output(17, GPIO.LOW)   ## Power oFF the LED 
 
 ### Toggle multiple times 
